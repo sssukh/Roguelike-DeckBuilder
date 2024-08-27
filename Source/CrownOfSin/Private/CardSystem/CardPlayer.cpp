5e343c2ea225c621608c76b6445b18855e039abe
namchanwoo <skacksdn00@naver.com>
2024-08-27 19:37:00 +0900

    Save local changes before merging

--- Source/CrownOfSin/Private/CardSystem/CardPlayer.cpp
+++ Source/CrownOfSin/Private/CardSystem/CardPlayer.cpp
@@ -5,7 +5,7 @@
 
 
 // Sets default values
-ACardPlayer::ACardPlayer()
+ACardPlayer::ACardPlayer(): ChanceManagerComponent(nullptr)
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
